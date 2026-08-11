a477ee251ca73b4fb7183d3b0db9ddcf59545ae1
JerryL <12345670310jerry@gmail.com>
2026-08-10 20:26:16 -0700

    Format content dates in UTC

--- src/data/site.ts
+++ src/data/site.ts
@@ -20,6 +20,7 @@ export const formatDate = (date: Date) =>
     year: 'numeric',
     month: '2-digit',
     day: '2-digit',
+    timeZone: 'UTC',
   }).format(date);
 
 export const normalizeList = (value: string | string[]) =>
